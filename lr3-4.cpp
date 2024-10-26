e40398d7a1f09682dda0c93900323d1f590b69d6
liana <lianabub07@gmail.com>
2024-10-26 09:11:00 +0300

    Add files via upload

---
+++ lr3-4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+int main() {
+	setlocale(LC_ALL, "RU");
+	int a;
+	cout << "Введите день недели";
+	cin >> a;
+	switch (a) {
+	case 1:
+		cout << "Понедельник";
+		case 2
+			cout << "Вторник";
+			case 3
+				cout << "Среда";
+				case 4
+					cout << "Четверг";
+					case 5
+						cout << "Пятница";
+						case 6
+							cout << "суббота";
+							case 7
+								cout << "Воскресенье";
+								break;
+	}
+	return 0;
